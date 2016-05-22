1a42fa87ee912cc8956b9774942e4ebd2ac0e070
Jonathan Yan <jyan93@stanford.edu>
2016-05-21 17:47:39 -0700

    created barebones node.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express3-handlebars');
+app.engine('handlebars',
+		exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-	res.send('Hello, World');
+	res.render('index', {
+		name: 'Jonathan'
+	});
+});
+
+app.get('/test', function(req, res){
+	res.render('test');
 });
 
 app.use('/public', express.static('public'));
