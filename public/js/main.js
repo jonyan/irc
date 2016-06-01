410ba3dea96c0f5e58b65ff64bff7b6776a08ca2
Jonathan Yan <jyan93@stanford.edu>
2016-05-31 22:24:26 -0700

    new profile

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
 
+	// Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+
+  $('select').material_select();
+
+
 	 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+
 
 	$('#cancel-ride-btn').click(function(e) {
 		e.preventDefault();
--- server.js
+++ server.js
@@ -13,28 +13,28 @@ storage = {"rides" :
 						{
 							"1" : {	"accepted" : false,
 								"month" : "June",
-								"date" : "6",
-								"pickupTime" : "3:30",
+								"date" : "2",
+								"pickupTime" : "2:00",
 								"pickupTimeMeridian" : "PM",
 								"duration" : "2",
-								"endTime" : "5:30",
+								"endTime" : "4:00",
 								"endTimeMeridian" : "PM",
 								"pickupLocationCity" : "San Jose",
-								"pickupLocationAddress" : "1210 S Bascom Ave, San Jose",
-								"dropoffLocationCity" : "Palo Alto",
-								"dropoffLocationAddress" : "333 W Julian St, Palo Alto",
+								"pickupLocationAddress" : "1800 Stokes St, San Jose",
+								"dropoffLocationCity" : "San Jose",
+								"dropoffLocationAddress" : "751 S Bascom Ave, San Jose",
 								"rideTitle" : "Social Services Appointment",
-								"clientName" : "Eduardo Saverin",
-								"clientPhone" : "(650) 293-1038",
-								"clientImage" : "/public/assets/eduardo.png",
-								"rideDistance" : "18",
+								"clientName" : "Ahmed Ali",
+								"clientPhone" : "(415) 343-2086",
+								"clientImage" : "/public/assets/ahmed.jpg",
+								"rideDistance" : "6",
 								"assistance" : "Assistance",
 								"riderAvailable" : "true",
-								"assistanceDescription" : "Eduardo needs volunteer assistance with filling out paper work and waiting in line."
+								"assistanceDescription" : "Ahmed needs volunteer assistance with translation and help filling out paper work."
 							},
 							"2" : {	"accepted" : false,
 								"month" : "June",
-								"date" : "7",
+								"date" : "4",
 								"pickupTime" : "3:30",
 								"pickupTimeMeridian" : "PM",
 								"duration" : "2",
@@ -108,6 +108,12 @@ app.post('/cancelRide', function(req, res) {
 
 app.get('/caseworker', function(req, res){
 	res.render('caseworker', {
+		rides: storage["rides"]
+	});
+});
+
+app.get('/form', function(req, res){
+	res.render('form', {
 		rides: storage["rides"]
 	});
 });
